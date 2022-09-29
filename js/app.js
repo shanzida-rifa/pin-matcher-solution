b5aa64dba2a45f800c5b499a720dc6e5c99709ee
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-29 22:17:15 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function getPin() {
+    const pin = generatePin();
+    const pinString = pin + '';
+    if (pinString.length === 4) {
+        return pin;
+    }
+    else {
+        console.log('pin is not 4 digit found', pin);
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const random = Math.round(Math.random() * 10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click', function () {
+    const pin = getPin();
+    // console.log(pin);
+    // display pin
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+})
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-number');
+    const previousTypedNumber = typedNumberField.value;
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedNumberField.value = '';
+        }
+        else if (number === '<') {
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+    else {
+
+        const newTypedNumber = previousTypedNumber + number;
+
+        typedNumberField.value = newTypedNumber;
+    }
+})
